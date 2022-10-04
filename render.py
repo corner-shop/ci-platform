cf0c0f5e677f458d0a6ef3e746cba9aaa9a770ec
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:57:38 +0000

    Adding numeric_owner as keyword arguement

--- render.py
+++ render.py
@@ -273,7 +273,7 @@ with tarfile.open(fileobj=f, mode='r:') as tar:
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(tar, target_folder)
